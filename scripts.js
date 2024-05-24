cd98a2c452d42ed5e65379a0a29d202409f71a3a
DaniCabrera91 <danielcabreravazquez91@gmail.com>
2024-05-24 20:38:46 +0200

    Updated the content of index.html and scripts.js files fixing issues whith de display of the images

--- scripts.js
+++ scripts.js
@@ -1,12 +1,11 @@
-
-
 const bearer_key = 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIzMjQ2NDllYmFmMjI2MzcxZjliY2JhMzZmMDliYTE0YSIsInN1YiI6IjY2NGUyOWZkMWRhNGZkODBjYzlhY2QxMiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.Pt5xSXkiYY2wzEqkGo7x-9O5Ehdz4czBnOX1qqFFY04'
 const BASE_URL = 'https://api.themoviedb.org/3/search/movie';
 const IMG_URL = 'https://image.tmdb.org/t/p/w500';
+const genresUrl = 'https://api.themoviedb.org/3/genre/movie/list';
 
 const searchMovie = async (event) => {
     event.preventDefault()
-   try {
+    try {
        const search = searchInput.value
        const options = {
            method: 'GET',
@@ -28,14 +27,9 @@ const searchMovie = async (event) => {
              console.error(error);
            });
     
-       }catch (error) {
+    }catch (error) {
     console.error(error)
     }
-}
-
-const getImages = async (event) => {
-    
-
 }
 
 const movieCard = document.querySelector('#movieCard')
@@ -47,14 +41,16 @@ const showMovies = (movies) => {
     movies.results.forEach((movie) => {
     movieCard.innerHTML += `
     <div class="card col-lg-3 col-xs-12 col-md-6 m-2">
-    <img src="${movie.poster_path}" alt="${movie.original_title}">
+    <img src="https://image.tmdb.org/t/p/w500/${movie.poster_path}" alt="${movie.original_title}">
     <div class="card-bod">
     <h3 class="card-header">${movie.original_title}</h3>
-    <h5 class="card-title">${movie.genres}</h5>
+    <h5 class="card-title">Genre: ${movie.genre_ids}</h5>
+    <p class="card-title">Release date: ${movie.release_date}</p>
+    <h4 class="card-header">Overview:</h4>
+
     <p class="card-title">${movie.overview}</p>
     </div>
-    </div>
-    `
+    </div>`
     })
 }
 
